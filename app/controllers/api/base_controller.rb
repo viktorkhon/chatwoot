86aed854f0dd0c08086a19bf8930340d2192a7fa
viktorkhon <GitMarine345>
2025-04-17 17:31:43 -0500

    Adding logs to debug 404 error

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -8,16 +8,18 @@ class Api::BaseController < ApplicationController
   private
 
   def authenticate_by_access_token?
+    Rails.logger.debug "Api::BaseController#authenticate_by_access_token? - Checking for access token"
     request.headers[:api_access_token].present? || request.headers[:HTTP_API_ACCESS_TOKEN].present?
   end
 
   def check_authorization(model = nil)
+    Rails.logger.debug "Api::BaseController#check_authorization - Checking authorization for model: #{model}"
     model ||= controller_name.classify.constantize
-
     authorize(model)
   end
 
   def check_admin_authorization?
+    Rails.logger.debug "Api::BaseController#check_admin_authorization? - Checking admin authorization"
     raise Pundit::NotAuthorizedError unless Current.account_user.administrator?
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,9 @@ class ApiController < ApplicationController
   skip_before_action :set_current_user, only: [:index]
 
   def index
+    Rails.logger.debug "ApiController#index - Starting health check"
+    Rails.logger.debug "ApiController#index - Redis status: #{redis_status}"
+    Rails.logger.debug "ApiController#index - PostgreSQL status: #{postgres_status}"
     render json: { version: Chatwoot.config[:version],
                    timestamp: Time.now.utc.to_fs(:db),
                    queue_services: redis_status,
@@ -11,15 +14,19 @@ class ApiController < ApplicationController
   private
 
   def redis_status
+    Rails.logger.debug "ApiController#redis_status - Checking Redis connection"
     r = Redis.new(Redis::Config.app)
     return 'ok' if r.ping
-  rescue Redis::CannotConnectError
+  rescue Redis::CannotConnectError => e
+    Rails.logger.error "ApiController#redis_status - Redis connection error: #{e.message}"
     'failing'
   end
 
   def postgres_status
+    Rails.logger.debug "ApiController#postgres_status - Checking PostgreSQL connection"
     ActiveRecord::Base.connection.active? ? 'ok' : 'failing'
-  rescue ActiveRecord::ConnectionNotEstablished
+  rescue ActiveRecord::ConnectionNotEstablished => e
+    Rails.logger.error "ApiController#postgres_status - PostgreSQL connection error: #{e.message}"
     'failing'
   end
 end

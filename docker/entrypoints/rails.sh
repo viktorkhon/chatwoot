a6dcfd5ea1b0a82f77f02ed3ea1ea10be410a32d
Viktor Khon <vkhon00@gmail.com>
2025-04-23 17:00:18 -0500

    Revert "Making updates to fic errors where container doesn't start"
    This reverts commit 644ea60c8db1cfd52007dde44d5071548d9efc94.

--- docker/entrypoints/rails.sh
+++ docker/entrypoints/rails.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
-set -x # Keep for debugging if needed
+
+set -x
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -rf /app/tmp/pids/server.pid
+rm -rf /app/tmp/cache/*
 
 echo "Waiting for postgres to become ready...."
-# If DATABASE_URL is set, parse out connection details.
-if [ -n "$DATABASE_URL" ]; then
-  export POSTGRES_HOST=$(echo "$DATABASE_URL" | sed -n 's#.*@\([^:/]\+\).*#\1#p')
-  export POSTGRES_PORT=$(echo "$DATABASE_URL" | sed -n 's#.*:\([0-9]\+\)/.*#\1#p')
-  export POSTGRES_USERNAME=$(echo "$DATABASE_URL" | sed -n 's#.*//\([^:]*\):.*#\1#p')
-fi
+
+# Let DATABASE_URL env take presedence over individual connection params.
+# This is done to avoid printing the DATABASE_URL in the logs
+$(docker/entrypoints/helpers/pg_database_url.rb)
 PG_READY="pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USERNAME"
-until $PG_READY; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 2
+
+until $PG_READY
+do
+  sleep 2;
 done
+
 echo "Database ready to accept connections."
 
-# Remove bundle install/check - should be done in Docker build
-# Remove db:chatwoot_prepare - should be done in release phase
+#install missing gems for local dev as we are using base image compiled for production
+bundle install
 
-# Remove pid file just in case
-rm -f /app/tmp/pids/server.pid
+BUNDLE="bundle check"
 
-# Execute the main process (passed as CMD)
+until $BUNDLE
+do
+  sleep 2;
+done
+
+# Execute the main process of the container
 exec "$@"

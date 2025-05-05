341cae645c49a1a7fb653163abfcf28f30e531de
viktorkhon <GitMarine345>
2025-05-05 18:00:46 -0500

    Saving page_url temporarily using Redis

--- app/builders/conversation_builder.rb
+++ app/builders/conversation_builder.rb
@@ -14,7 +14,42 @@ class ConversationBuilder
   end
 
   def create_new_conversation
-    ::Conversation.create!(conversation_params)
+    conversation_params_with_page_info = conversation_params.dup
+    
+    # Check if custom_attributes already has page URL info
+    custom_attributes = conversation_params_with_page_info[:custom_attributes] || {}
+    string_keys_custom_attrs = custom_attributes.stringify_keys
+    
+    if string_keys_custom_attrs['page_url'].blank?
+      # Try to get page info from Redis cache using contact_inbox source_id
+      redis_key = "contact_inbox:#{@contact_inbox.source_id}:page_info"
+      
+      $alfred.with do |conn|
+        cached_page_info = conn.get(redis_key)
+        
+        if cached_page_info.present?
+          begin
+            # Parse the cached page info
+            page_info = JSON.parse(cached_page_info, symbolize_names: true)
+            
+            # Log that we found cached page info
+            Rails.logger.info "ConversationBuilder - Found cached page info for #{@contact_inbox.source_id}: #{page_info}"
+            
+            # Add the page info to custom_attributes using string keys
+            string_keys_custom_attrs['page_url'] = page_info[:page_url]
+            string_keys_custom_attrs['page_title'] = page_info[:page_title]
+            string_keys_custom_attrs['referer_url'] = page_info[:referer_url]
+            
+            # Update custom_attributes in the conversation params
+            conversation_params_with_page_info[:custom_attributes] = string_keys_custom_attrs
+          rescue => e
+            Rails.logger.error "ConversationBuilder - Error parsing cached page info: #{e.message}"
+          end
+        end
+      end
+    end
+    
+    ::Conversation.create!(conversation_params_with_page_info)
   end
 
   def conversation_params

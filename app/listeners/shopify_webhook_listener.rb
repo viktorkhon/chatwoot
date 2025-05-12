bd94bb0ba2a8c0607910255d930ba04e54ef677f
Viktor Khon <vkhon00+git@gmail.com>
2025-05-11 20:33:56 -0500

    Adding debug logs to webhook listener

--- app/listeners/shopify_webhook_listener.rb
+++ app/listeners/shopify_webhook_listener.rb
@@ -2,8 +2,11 @@ class ShopifyWebhookListener < BaseListener
   include Singleton
   
   def account_shopify_name_updated(event)
+    Rails.logger.info "DEBUG: Entered ShopifyWebhookListener#account_shopify_name_updated with event: \\#{event.inspect}"
     shopify_name_change = event.data[:shopify_name_change]
     account = event.data[:account]
+
+    Rails.logger.info "DEBUG: shopify_name_change: \\#{shopify_name_change.inspect}, account: \\#{account.inspect}"
 
     return if shopify_name_change.blank? || account.blank?
 
@@ -17,11 +20,12 @@ class ShopifyWebhookListener < BaseListener
     
     # Log the webhook URL from ENV
     webhook_url = ENV.fetch('N8N_SHOPIFY_WEBHOOK_URL', nil)
-    Rails.logger.info "🔔 ShopifyWebhookListener: N8N_SHOPIFY_WEBHOOK_URL is set to: #{webhook_url || 'nil'}"
+    Rails.logger.info "🔔 ShopifyWebhookListener: N8N_SHOPIFY_WEBHOOK_URL is set to: \\#{webhook_url || 'nil'}"
 
     # Only trigger if there's an actual change
     return if previous_value == current_value
 
+    Rails.logger.info "DEBUG: Calling Shopify::WebhookService.new(account: account).send_shopify_name_update(previous_value)"
     Shopify::WebhookService.new(account: account).send_shopify_name_update(previous_value)
   end
 end 
